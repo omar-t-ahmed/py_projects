8b296fd6536a836e2f13a938ea8aecb941deccec
omar-t-ahmed <omartahmed@gmail.com>
2024-02-19 18:11:46 -0500

    add error handling

--- num_guesser.py
+++ num_guesser.py
@@ -4,4 +4,14 @@ print('welcome to NumGuess!')
 
 upper_bound = input('Type a number: ')
 
+if upper_bound.isdigit():
+    upper_bound = int(upper_bound)
+
+    if upper_bound <= 0:
+        print('please type a number greater than 0')
+        quit()
+else:
+    print('please type a number')
+    quit()
+
 random_num = random.randint(1, upper_bound)
