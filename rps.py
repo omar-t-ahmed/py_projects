8ae65600c4b23765eb28c825c3d7a266b18acca0
omar-t-ahmed <omartahmed@gmail.com>
2024-02-26 17:18:02 -0500

    add number func

--- rps.py
+++ rps.py
@@ -4,4 +4,14 @@ user_wins = 0
 computer_wins = 0
 
 while True:
-    user_input = input('Type Rock/Paper/Scissors or Q to quit')
+    user_input = input('Type Rock/Paper/Scissors or Q to quit: ').lower()
+    if user_input == 'q':
+        quit()
+
+    if user_input not in ['rock','paper','scissors']:
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+
+print('Goodbye!')
