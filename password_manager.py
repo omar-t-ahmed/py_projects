bab2f6065756527b0dcbd21ced3a972bb4a94c99
omar-t-ahmed <omartahmed@gmail.com>
2024-02-29 21:29:46 -0500

    setup pwd manager

---
+++ password_manager.py
@@ -0,0 +1,10 @@
+pwd = input('what is the master password? ')
+
+mode = input('would you like to add a new password or view existing ones (view, add)? ')
+while True:
+    if mode == 'view':
+        pass
+    elif mode == 'add':
+        pass
+    else:
+        print('invalid mode')
