3a9b667f16fcd75d9b5dbadd2b3939da32040d9a
omar-t-ahmed <omartahmed@gmail.com>
2024-03-05 14:57:40 -0500

    add sleep

--- reminder.py
+++ reminder.py
@@ -3,3 +3,10 @@ import time
 remind = input('Hi Im RemindBot. What should I remind you about? ')
 
 min = float(input('In how many minutes? '))
+
+sec = min * 60
+
+time.sleep(sec)
+
+print(remind)
+
