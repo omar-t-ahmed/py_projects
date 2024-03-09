407a466ad642061fcaae77e8187b7cef76f95f8e
omar-t-ahmed <omartahmed@gmail.com>
2024-03-08 23:46:32 -0500

    add read

--- password_manager.py
+++ password_manager.py
@@ -3,7 +3,9 @@ master_pwd = input('what is the master password? ')
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            user, passw = data.split('|')
+            print (f'User: {user} Password: {passw}')
 
 def add():
     name  = input('Account name: ')
