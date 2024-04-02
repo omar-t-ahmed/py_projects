3a1b81bd41fa4bb00a4d2426843f6c0c5225933a
omar-t-ahmed <omartahmed@gmail.com>
2024-04-01 23:00:24 -0400

    to do list

---
+++ to_do.py
@@ -0,0 +1,23 @@
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+        else:
+            print("Task not found in the list.")
+
+    def display_tasks(self):
+        if self.tasks:
+            print("Tasks:")
+            for index, task in enumerate(self.tasks, start=1):
+                print(f"{index}. {task}")
+        else:
+            print("No tasks in the list.")
+
+# if __name__ == "__main__":
+#     main()
