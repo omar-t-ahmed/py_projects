41ebbf00c73110f449027a2066428beab3bfe005
omar-t-ahmed <omartahmed@gmail.com>
2024-04-18 22:46:54 -0400

    add convert func

--- exchange_rate.py
+++ exchange_rate.py
@@ -5,3 +5,10 @@ def get_exchange_rate(base_currency, target_currency):
         'GBP': 0.82,
         'JPY': 133.5
     }
+    usd_to_base_rate = 1 / rates[base_currency]
+    base_to_target_rate = usd_to_base_rate * rates[target_currency]
+    return base_to_target_rate
+
+def convert_currency(amount, base_currency, target_currency):
+    rate = get_exchange_rate(base_currency, target_currency)
+    return amount * rate
