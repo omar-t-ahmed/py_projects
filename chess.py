ef5e70a14ff94e58dedaa0e61ad404b29b544036
omar-t-ahmed <omartahmed@gmail.com>
2024-04-27 14:04:46 -0400

    add pieces

--- chess.py
+++ chess.py
@@ -2,3 +2,21 @@ class Piece:
     def __init__(self, color, name):
         self.color = color
         self.name = name
+
+class Pawn(Piece):
+    pass
+
+class Rook(Piece):
+    pass
+
+class Knight(Piece):
+    pass
+
+class Bishop(Piece):
+    pass
+
+class Queen(Piece):
+    pass
+
+class King(Piece):
+    pass
