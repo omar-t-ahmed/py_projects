fe415c5eded08ddd7e017565806abfbf9fedcff2
omar-t-ahmed <omartahmed@gmail.com>
2024-05-02 14:56:02 -0400

    add setup

--- chess.py
+++ chess.py
@@ -23,5 +23,20 @@ class King(Piece):
 
 class Board:
         def __init__(self):
-        self.board = [[None for _ in range(8)] for _ in range(8)]
-        self.setup_pieces()
+            self.board = [[None for _ in range(8)] for _ in range(8)]
+            self.setup_pieces()
+
+        def setup_pieces(self):
+            for i in range(8):
+                self.board[1][i] = Pawn('black', 'P')
+                self.board[6][i] = Pawn('white', 'P')
+            self.board[0][0] = self.board[0][7] = Rook('black', 'R')
+            self.board[7][0] = self.board[7][7] = Rook('white', 'R')
+            self.board[0][1] = self.board[0][6] = Knight('black', 'N')
+            self.board[7][1] = self.board[7][6] = Knight('white', 'N')
+            self.board[0][2] = self.board[0][5] = Bishop('black', 'B')
+            self.board[7][2] = self.board[7][5] = Bishop('white', 'B')
+            self.board[0][3] = Queen('black', 'Q')
+            self.board[0][4] = King('black', 'K')
+            self.board[7][3] = Queen('white', 'Q')
+            self.board[7][4] = King('white', 'K')
