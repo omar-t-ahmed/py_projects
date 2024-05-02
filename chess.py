af915a7b1bd325327f900632225dd6998e46c279
omar-t-ahmed <omartahmed@gmail.com>
2024-05-02 14:57:16 -0400

    add main

--- chess.py
+++ chess.py
@@ -40,7 +40,11 @@ class Board:
             self.board[0][4] = King('black', 'K')
             self.board[7][3] = Queen('white', 'Q')
             self.board[7][4] = King('white', 'K')
-            
+
         def print_board(self):
             for row in self.board:
                 print(' '.join([piece.name if piece else '.' for piece in row]))
+
+if __name__ == '__main__':
+    chess_board = Board()
+    chess_board.print_board()
