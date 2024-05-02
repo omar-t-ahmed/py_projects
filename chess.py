f557aed47cebbdeea5368cad647a45f7dbd99467
omar-t-ahmed <omartahmed@gmail.com>
2024-05-02 14:55:28 -0400

    add board

--- chess.py
+++ chess.py
@@ -20,3 +20,8 @@ class Queen(Piece):
 
 class King(Piece):
     pass
+
+class Board:
+        def __init__(self):
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+        self.setup_pieces()
