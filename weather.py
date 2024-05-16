99ff1c2cd400a8df64942389a96ac079462f0a92
omar-t-ahmed <omartahmed@gmail.com>
2024-05-16 14:27:11 -0400

    add weather fetch

--- weather.py
+++ weather.py
@@ -1,3 +1,15 @@
 import requests
 import json
 import sys
+
+API_KEY = 'your_openweathermap_api_key'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+def get_weather(city_name):
+    params = {
+        'q': city_name,
+        'appid': API_KEY,
+        'units': 'metric'  # Use 'imperial' for Fahrenheit
+    }
+    response = requests.get(BASE_URL, params=params)
+    return response.json()
