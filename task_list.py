f4101725da0c97e95c34119bbc2b8a8ef7f38c44
omar-t-ahmed <omartahmed@gmail.com>
2024-06-07 15:07:55 -0400

    add remove task

--- task_list.py
+++ task_list.py
@@ -5,3 +5,10 @@ class TaskList:
     def add_task(self, task):
         self.tasks.append(task)
         print(f"Task '{task}' added.")
+    
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            print(f"Task '{task}' removed.")
+        else:
+            print(f"Task '{task}' not found.")
