52f1f838b0b6cffbb59f97ce5d3b2298d18b8f66
omar-t-ahmed <omartahmed@gmail.com>
2024-07-12 18:27:46 -0400

    rework queen piece

--- chess.py
+++ chess.py
@@ -81,24 +81,6 @@ class Bishop(Piece):
         return moves
 
 class Queen(Piece):
-    def get_moves(self, position, board):
-        moves = []
-        x, y = position
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1),
-                    (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            while 0 <= nx < 8 and 0 <= ny < 8:
-                if board[nx][ny] is None:
-                    moves.append((nx, ny))
-                elif board[nx][ny].color != self.color:
-                    moves.append((nx, ny))
-                    break
-                else:
-                    break
-                nx += dx
-                ny += dy
-        return moves
 
 class King(Piece):
     def get_moves(self, position, board):
